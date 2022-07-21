e6869f22a3b48ca4e195bc6f67ffe712c279d383
pradhan21 <75258017+pradhan21@users.noreply.github.com>
2022-07-21 16:09:10 +0545

    aa

--- one-music-gh-pages/reg_handle.php
+++ one-music-gh-pages/reg_handle.php
@@ -1,20 +1,30 @@
 <?php
     
     include ('connection.php');
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $cpassword = $_POST['cpassword'];
-    $address = $_POST['address'];
-
-    $sql="INSERT INTO user(fname,lname,email,password,cpassword,address) VALUES('$fname', '$lname','$email','$password','cpassword','$address')";
-
-    if(mysqli_query($conn,$sql)){
-       header("location:login.php");
+    if(isset($_POST['submit'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $password = md5($_POST['password']);
+        $cpassword = md5($_POST['cpassword']);
+        $address = $_POST['address'];
+        if($password===$cpassword){
+            $sql="INSERT INTO user(fname,lname,email,password,cpassword,address) VALUES('$fname', '$lname','$email','$password','cpassword','$address')";
+        
+            if(mysqli_query($conn,$sql)){
+                header("location:login.php");
+            }
+            else{
+                echo "Error: ".mysqli_error($conn);
+            }
+        }
+        else{
+            echo mysqli_error($conn);
+            header("location:register.php");
+        }   
     }
     else{
-        echo "Error: ".mysqli_error($conn);
+        echo mysqli_error($conn);
     }
     
 

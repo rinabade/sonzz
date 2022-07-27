effd2ee3bee4246317e8f12f3c64ef5e0b24779b
aditistha <sthaaditi1@gmail.com>
2022-07-27 14:32:08 +0545

    changes in artist album songs.js songpic.css

--- one-music-gh-pages/albumpic.php
+++ one-music-gh-pages/albumpic.php
@@ -78,17 +78,17 @@
                         
                         <div class="multi">
                             <form action="#">
-                        <input class="file-input" type="file" name="file" hidden>
-                        <i class="bi bi-file-earmark-arrow-up"></i>
+                             Choose songs<input class="file-input" type="file" name="file" hidden>
+                        
                          </form>
                         <section class="progress-area"></section>
                         <section class="uploaded-area"></section>
-                       </div>
+                       </div><br>
                    <label>Image</label><br>
                         <div class="choose"> 
 					Choose image<input type="file" name="img" class="file">
 					
-				   </div><br><br>
+				   </div><br>
                    <div class="open">
                 <button class="button button2"> Upload </button><br><br>
               </div>
@@ -98,7 +98,7 @@
         
               <!-- change -->
              
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+              <script src="js/songs.js"></script>
 </div>
 </header>
 </body>
